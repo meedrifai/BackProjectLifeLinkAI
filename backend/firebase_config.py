4368ac7f5d88b26cb31f3cdfa374353725f31312
meedrifai <rifaii.mohameed@gmail.com>
2025-05-31 12:10:35 +0100

    firebase_config

--- backend/firebase_config.py
+++ backend/firebase_config.py
@@ -2,5 +2,10 @@
 import firebase_admin
 from firebase_admin import credentials
 
+# Charger les credentials depuis le fichier JSON
 cred = credentials.Certificate("credentials.json")
-firebase_admin.initialize_app(cred, {"databaseURL" : "https://blood-3fda1-default-rtdb.firebaseio.com/"})
+
+# Initialiser l'application Firebase avec les credentials et l'URL de la base de données
+firebase_admin.initialize_app(cred, {
+    "databaseURL": "https://blood-3fda1-default-rtdb.firebaseio.com/"
+})
